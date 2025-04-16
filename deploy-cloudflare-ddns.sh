cbf23dc7e5232fc156b06f8e25f95b20904e9266
Freddy De Abreu <pidacd@gmail.com>
2025-04-16 19:30:20 +0200

    Update deploy-cloudflare-ddns.sh

--- deploy-cloudflare-ddns.sh
+++ deploy-cloudflare-ddns.sh
@@ -53,6 +53,7 @@ fi
 # ========================
 # Crear contenedor automáticamente
 # ========================
+CTID=$(pvesh get /cluster/nextid)
 pct create $CTID local:vztmpl/${TEMPLATE} \
   -hostname cloudflare-stack \
   -storage ${DETECTED_STORAGE} \
@@ -61,7 +62,7 @@ pct create $CTID local:vztmpl/${TEMPLATE} \
   -cores ${var_cpu} \
   -net0 name=eth0,bridge=vmbr0,ip=dhcp \
   -unprivileged ${var_unprivileged} \
-  -features nesting=1 \
+  -features nesting=1
   -onboot 1
 
 pct start $CTID
